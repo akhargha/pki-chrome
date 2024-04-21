9c248b8a0080a463e26e5f24ad54c0257020ab09
Anupam Khargharia <kharghariaanupam07@gmail.com>
2024-04-21 10:53:26 -0400

    Testing branch

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onStartup.addListener(function() {
     chrome.storage.local.set({ sessionList: {} }, function() {
-        console.log("Session list cleared on startup");
+        console.log("Session list cleared on startup.");
     });
 });
