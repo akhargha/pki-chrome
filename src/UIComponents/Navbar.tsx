de2ce7be7bd49ba1f98c91bd0b760d35f983df7e
Anupam Khargharia <kharghariaanupam07@gmail.com>
2024-09-22 17:52:10 -0400

    initialize points to 0 || points/-1 and not 0/-1

--- src/UIComponents/Navbar.tsx
+++ src/UIComponents/Navbar.tsx
@@ -1,76 +1,91 @@
-/* eslint-disable jsx-a11y/role-supports-aria-props */
 import { Component } from 'react';
 import { animated, Spring, SpringValue } from 'react-spring';
 import { ExtensionSettings } from '../types/Settings';
+
 interface NavbarProps {
   toggleSensitiveSiteControls: () => void;
   viewState: 0 | 1;
-
   settings: ExtensionSettings;
   points: number;
   group: number;
 }
+
 interface NavbarState {
   burgerOpen: boolean;
   animationDone: boolean;
-  points: number | null;
+  points: number;
+  group: number;
 }
+
 class Navbar extends Component<NavbarProps, NavbarState> {
   constructor(props: NavbarProps) {
-    super(props, {});
+    super(props);
 
-    // const props = useSpring({ opacity: 1, from: { opacity: 0 } })
     this.state = {
       burgerOpen: false,
       animationDone: true,
-      points: null
+      points: props.points,
+      group: props.group
     };
   }
+
   componentDidMount() {
-    chrome.storage.local.get({ _pki_userData: { group: null } }, (data) => {
-      const group = data._pki_userData.group;
-      const points = group === 1 ? 0 : -1;
-      console.log('points', points);
-      this.setState({ points });
-    });
+    this.updatePointsFromStorage();
+    chrome.storage.onChanged.addListener(this.handleStorageChange);
   }
+
+  componentWillUnmount() {
+    chrome.storage.onChanged.removeListener(this.handleStorageChange);
+  }
+
+  handleStorageChange = (changes: { [key: string]: chrome.storage.StorageChange }, areaName: string) => {
+    if (areaName === 'local' && changes.Points) {
+      this.setState({ points: changes.Points.newValue });
+    }
+    if (areaName === 'local' && changes._pki_userData) {
+      const newUserData = changes._pki_userData.newValue;
+      if (newUserData && newUserData.group !== undefined) {
+        this.setState({ group: newUserData.group });
+      }
+    }
+  };
+
+  updatePointsFromStorage = () => {
+    chrome.storage.local.get(['_pki_userData', 'Points'], (data) => {
+      const userData = data._pki_userData || {};
+      const group = userData.group !== undefined ? userData.group : this.state.group;
+      const points = group === 1 ? (data.Points || 0) : -1;
+      this.setState({ points, group });
+    });
+  };
 
   handleAnimationRest = () => {
     this.setState({ animationDone: true });
   };
+
   render() {
     return (
       <nav className='navbar' role='navigation' aria-label='main navigation'>
         <div className='navbar-brand' style={{ zIndex: 5, position: 'fixed' }}>
           <a className='navbar-item' href='https://trincoll.edu'>
             <span style={{ fontSize: '2em' }}>🐟</span>
-            {/* <!-- <img src="/icons/shield-halved-solid.svg" width="40" height="28" /> --> */}
           </a>
-          {/* <!--POINTS--> */}
-          {
-            this.state.points !== -1 ? (
-              <h2 style={{ marginLeft: '3em', marginTop: '0.8em' }} id='points'>
-                Points: <span>{this.state.points}</span>
-              </h2>
-            ) : (
-              <h2 style={{ marginLeft: '3em', marginTop: '0.8em' }} id='points'>
-                Points: <span>N/A</span>
-              </h2>
-            )
-          }
+          {this.state.group === 1 ? (
+            <h2 style={{ marginLeft: '3em', marginTop: '0.8em' }} id='points'>
+              Points: <span>{this.state.points}</span>
+            </h2>
+          ) : (
+            <h2 style={{ marginLeft: '3em', marginTop: '0.8em' }} id='points'>
+              Points: <span>N/A</span>
+            </h2>
+          )}
 
           <img
             src='./icons/gear-solid.svg'
-            className={`navbar-burger burger ${this.state.burgerOpen || !this.state.animationDone
-              ? 'is-active'
-              : ''
+            className={`navbar-burger burger ${this.state.burgerOpen || !this.state.animationDone ? 'is-active' : ''
               }`}
             aria-label='menu'
-            aria-expanded={
-              this.state.burgerOpen || !this.state.animationDone
-                ? 'true'
-                : 'false'
-            }
+            aria-expanded={this.state.burgerOpen || !this.state.animationDone ? 'true' : 'false'}
             data-target='navbarMenu'
             onClick={() => {
               const original = this.state.burgerOpen;
@@ -80,38 +95,28 @@ class Navbar extends Component<NavbarProps, NavbarState> {
               marginLeft: '55%',
               justifyContent: 'right',
               cursor: 'pointer',
-              width: '5vh', // Adjust the width to match the button size
-              height: 'auto', // Adjust the height to match the button size
+              width: '5vh',
+              height: 'auto',
               maxWidth: '50px',
               outline: 'none',
               border: 'none'
             }}
-          >
-            {/* <span aria-hidden='true'></span>
-            <span aria-hidden='true'></span>
-            <span aria-hidden='true'></span> */}
-          </img>
+          />
         </div>
         <Spring
           from={{
-            transform: this.state.burgerOpen
-              ? 'translateY(-100%)'
-              : 'translateY(0%)'
+            transform: this.state.burgerOpen ? 'translateY(-100%)' : 'translateY(0%)'
           }}
           to={{
-            transform: this.state.burgerOpen
-              ? 'translateY(0%)'
-              : 'translateY(-100%)'
+            transform: this.state.burgerOpen ? 'translateY(0%)' : 'translateY(-100%)'
           }}
           onRest={this.handleAnimationRest}
         >
-          {(props: { transform: SpringValue<string>; }) => (
+          {(props: { transform: SpringValue<string> }) => (
             <animated.div
               style={{ ...props, zIndex: -1, position: 'fixed', top: '50px' }}
               id='navbarMenu'
-              className={`navbar-menu ${this.state.burgerOpen || !this.state.animationDone
-                ? 'is-active'
-                : ''
+              className={`navbar-menu ${this.state.burgerOpen || !this.state.animationDone ? 'is-active' : ''
                 }`}
             >
               <div className='navbar-end'>
@@ -135,15 +140,12 @@ class Navbar extends Component<NavbarProps, NavbarState> {
                     <label className='checkbox'>
                       <input
                         type='checkbox'
-                        checked={
-                          this.props.settings.autoSearchEnabled ? true : false
-                        }
+                        checked={this.props.settings.autoSearchEnabled}
                         id='auto-search-checkbox'
-                        onClick={() => {
-                          console.log('clicked');
+                        onChange={() => {
+                          console.log('Auto-search checkbox clicked');
                           chrome.storage.local.set({
-                            autoSearchEnabled:
-                              !this.props.settings.autoSearchEnabled
+                            autoSearchEnabled: !this.props.settings.autoSearchEnabled
                           });
                         }}
                       />
